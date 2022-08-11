bf58c68ebafe5acbc999d4c9563927cc9a0a7103
Fgabriel <04894Gl>
2022-08-11 16:53:32 -0300

    adaptando layout mobile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
 
 const routes: Routes = [
+  {path:'' , component: HomeComponent},
   {path:'servidores' , loadChildren: ()=>import('./pages/servidores/servidores.module').then(C=>C.ServidoresModule)},
   {path:'setores' , loadChildren: ()=>import('./pages/setores/setores.module').then(E=>E.SetoresModule)}
 ];
